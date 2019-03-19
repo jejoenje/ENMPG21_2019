559958ee2a8832181316e10ff486f15f2e973c43
jejoenje <jeroenminderman@gmail.com>
2019-03-19 18:34:55 +0000

    slowly getting there?

--- Convert_to_pdf_LOCAL_5547.R
+++
@@ -1,6 +0,0 @@
-install.packages("webshot")
-library(webshot)
-install_phantomjs()
-file_name <- paste0("file://", normalizePath("ENMPG21_JM_ModellingIntro_EXCERCISE.html"))
-webshot(file_name, "excercise_1.pdf")
-  
--- Convert_to_pdf_REMOTE_5547.R
+++
@@ -1,5 +0,0 @@
-install.packages("webshot")
-library(webshot)
-install_phantomjs()
-file_name <- paste0("file://", normalizePath("ENMPG21_JM_ModellingIntro.html"))
-webshot(file_name, "mypdf.pdf")
